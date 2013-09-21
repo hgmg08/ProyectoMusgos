9e8705ffcaef4a732ffabf3920073fd0a335c90c
hgmg08 <hgmg08@gmail.com>
2013-09-21 12:42:49 -0430

    Update

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -43,7 +43,7 @@ class Home extends CI_Controller {
 		$this->twiggy->set('sustratos', json_encode($this->em->getRepository("entities\Sustrato")->getAll()));
 		$this->twiggy->set('ecorregiones', json_encode($ecor = $this->em->getRepository("entities\Ecorregion")->getAll()));
 		$this->twiggy->set('ecosistemas', json_encode($this->em->getRepository("entities\Ecosistema")->getAll()));
-		$this->twiggy->set('estados', json_encode($this->em->getRepository("entities\Estado")->getAll()))
+		$this->twiggy->set('estados', json_encode($this->em->getRepository("entities\Estado")->getAll()));
 		$this->twiggy->title('Musgos de Venezuela')->append("Acerca de");
 		$this->twiggy->template('main/about')->display();
 	}
